67ae8b682a9a3e9824e51b4e116d4f527279f1a8
Muzammil Hussain <muzammil.org73@gmail.com>
2026-02-19 21:39:06 +0500

    Add files via upload

---
+++ prog1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a=5;
+    cout <<" the value of a :"<<a<<endl;
+    cout <<"the adrress of a is:"<<&a<<endl;// & this operater is used to display the addresss of the variable
+    return 0;
+}
