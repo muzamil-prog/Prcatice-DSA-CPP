62324c6d66d3ee891a7023705eb892dd1d9d3258
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-08 23:30:50 +0500

    Add files via upload

---
+++ prog13.js
@@ -0,0 +1,9 @@
+console.log("check the numbr is postive zero or negitve");
+var number= 22;
+if(number > 0){
+    console.log("the number is postive " +number);
+}else if(number == 0){
+    console.log("the number is zero:"+number);
+}else{
+    console.log("the number is negtive:"+number);
+}
