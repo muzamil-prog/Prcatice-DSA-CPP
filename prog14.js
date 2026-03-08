c354fb7a7f50487599cdad5a393c964559ec1f5a
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-08 23:32:02 +0500

    Add files via upload

---
+++ prog14.js
@@ -0,0 +1,18 @@
+console.log("print ont two ten numbers")
+
+for(let i=0; i<10; i++){
+    console.log(i);
+}
+
+
+console.log("prints all even or odd numbers :")
+for(let i=1; i<=20; i++){
+    if(i%2==0){
+        console.log("this even numbr")
+        console.log(i);
+    }else if(i%2 !=0){
+        console.log("this is oddd numbers")
+        console.log(i)
+    }
+        
+    }
