bf9dbc1f04501d1c5992c1d29fcfbc5e9d07f843
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-11 22:56:33 +0500

    Add files via upload

---
+++ pyramidpattern.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+class pyramidpattern{
+    public:
+    void show(){
+        int n=4;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-i-1; j++){
+                cout <<"  ";
+             
+            }
+               for(int j=0; j<i+1; j++){
+                cout <<j<<" ";
+            }
+             for( int j=i; j>0; j--){
+                cout <<j<<" ";
+             }
+             cout <<endl;
+        }
+    }
+};
+int main(){
+    pyramidpattern obj;
+    obj.show();
+    return 0;
+}
