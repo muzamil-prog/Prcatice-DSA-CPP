d065669b1628ae483a6c61b2c855c767a116c964
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-13 23:47:01 +0500

    Add files via upload

---
+++ squarPattern.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+class squarePattern{
+    public:
+    
+    void show(){
+        int n=4;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                cout <<" * ";
+            }
+            cout <<endl;
+        }
+    }
+};
+int main(){
+    squarePattern obj;
+    obj.show();
+    return 0;
+}
