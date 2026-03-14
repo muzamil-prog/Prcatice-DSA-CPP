d1ca50c761a5f739e185b16e5e67f98c7a4210b3
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-14 20:02:23 +0500

    Add files via upload

---
+++ trianglepatern3.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class squarePatern{
+    public:
+    void show(){
+        int n=4;
+        int num=0;
+        for(int i=0; i<n; i++){
+        
+            for( int j=0; j<i+1; j++){
+                cout <<i+1<<" ";
+                
+                
+
+            }
+            cout <<endl;
+        }
+    }
+};
+int main (){
+  squarePatern obj;
+  obj.show(); 
+    return 0;
+}
