47394e47c1c71988be1071f3e93d25295407a31e
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-24 20:39:30 +0500

    Add files via upload

--- prog36.cpp
+++ prog36.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-class squarePattern{
+class pairSum{
     public:
     
-    void show(){
-        int n=4;
+    vector<int> pairsum(vector<int> &nums, int & target){
+        int n=nums.size();
+        vector<int> ans;
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                cout <<j<<" ";
+            for(int j=i+1; j<n; j++){
+                if(nums[i]+nums[j]==target){
+                 ans.push_back(i);
+                 ans.push_back(j);
+                }
             }
-            cout <<endl;
         }
+        return ans;
     }
 };
 int main(){
-    squarePattern obj;
-    obj.show();
-    return 0;
+    vector<int> nums={2,3,4,5,6,7};
+    int target=11;
+    pairSum obj;
+   vector<int> ans= obj.pairsum(nums, target);
+
+   // show index 
+    cout <<ans[0]<<" seconde"<<ans[1]<<endl;
 }
