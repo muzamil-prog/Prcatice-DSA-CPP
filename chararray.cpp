d75312e2f737941f8f61a0403276aa5768766396
Muzammil Hussain <muzammil.org73@gmail.com>
2026-03-31 23:05:45 +0500

    Add files via upload

---
+++ chararray.cpp
@@ -0,0 +1,9 @@
+// jb tk hmm string ma /'0' add nhi krty wo valid string nhi 
+#include <iostream>
+#include<string>
+using namespace std;
+int main(){
+    char str[]={"hello "};// string letral yani constant
+ cout <<str<<endl;
+ return 0;
+}
