11538c1346d2fbb86d17cbe68cb133455e3b4add
Muzammil Hussain <muzammil.org73@gmail.com>
2026-05-16 23:52:28 +0500

    Add files via upload

---
+++ Tree.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+class Node{
+    public:
+    Node * left;
+    Node * right;
+    int data;
+    Node(int data){
+        this->data=data;
+        left=right=NULL;
+    }
+};
+class Tree{
+d    void  preeorder(int data){
+        Node *root=new Node(data);
+        root->left=new Node()
+
+    }
+}
