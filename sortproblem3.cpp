f6d7dbb3f1dff79e5cb7a46a5dc86ff6477db3e6
Muzammil Hussain <muzammil.org73@gmail.com>
2026-06-16 22:54:32 +0500

    Add files via upload

---
+++ sortproblem3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+class insert{
+    public:
+    void sort(int arr[5], int size){
+        for(int i=0; i<size; i++){
+            int key=i+1;
+            int prev=i;
+            while()
+        }
+    }
+}
