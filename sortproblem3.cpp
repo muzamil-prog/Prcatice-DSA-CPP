ea9b0a9ae2c3ed757329a246a104900e9eaea00c
Muzammil Hussain <muzammil.org73@gmail.com>
2026-06-17 23:49:31 +0500

    Add files via upload

--- sortproblem3.cpp
+++ sortproblem3.cpp
@@ -5,8 +5,27 @@ class insert{
     void sort(int arr[5], int size){
         for(int i=0; i<size; i++){
             int key=i+1;
-            int prev=i;
-            while()
+            int current=i;
+            while(key>0 && arr[current]> arr[key]){
+                arr[key+1]=arr[key];
+            }
+
+            key[key+1]=current;
         }
     }
+    void print(int arr[5], int size){
+        for(int i=0; i<n; i++){
+            cout <<i<<" ";
+        }
+        cout <<endl;
+    }
+};
+int main()
+{
+    insert obj;
+    int arr[5]={3,1,2,5,4};
+    int size=5;
+    obj.sort(arr, size);
+    obj.print(arr, size);
+    return 0;
 }
