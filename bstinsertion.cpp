8bbc966c3b5d88e49c6f1696022677abc072f425
Muzammil Hussain <muzammil.org73@gmail.com>
2026-07-05 23:56:53 +0500

    Add files via upload

---
+++ bstinsertion.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+
+public:
+
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int d){
+
+        data=d;
+        left=right=NULL;
+
+    }
+
+};
+
+Node* insert(Node* root,int value){
+
+    if(root==NULL)
+        return new Node(value);
+
+    if(value<root->data)
+        root->left=insert(root->left,value);
+    else
+        root->right=insert(root->right,value);
+
+    return root;
+
+}
+
+void inorder(Node* root){
+
+    if(root){
+
+        inorder(root->left);
+        cout<<root->data<<" ";
+        inorder(root->right);
+
+    }
+
+}
+
+int main(){
+
+    Node* root=NULL;
+
+    root=insert(root,50);
+    insert(root,30);
+    insert(root,70);
+    insert(root,20);
+    insert(root,40);
+
+    inorder(root);
+
+}
